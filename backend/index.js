dcb6734b4f68387747bdaa8658b325bcb9ab2cb6
pachecoedgardo2006 <pachecoedgardo2006@gmail.com>
2026-07-19 15:11:58 -0500

    Ajuste forzado en app.js y vercel.json

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
+import 'dotenv/config';
 import app from './src/app.js';
 
-// Para Vercel, simplemente exportamos la app configurada.
-// Vercel se encarga de escuchar en el puerto automáticamente.
 export default app;
